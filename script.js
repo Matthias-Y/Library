fd4c8bfaa43bbaa1b4e3a7c4f1007648a0efbbcc
Matthew Yang <bossman@Matthews-Laptop.local>
2023-08-06 16:38:54 -0700

    Extract form contents after form validation

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function Book(author, title, pages, isRead){
     this.isRead = isRead;
 }
 
-function addBookToLibrary(Book){
-    myLibrary.push(Book);
+function addBookToLibrary(book){
+    myLibrary.push(book);
 }
 
 function removeBookFromLibrary() {
@@ -17,7 +17,8 @@ function removeBookFromLibrary() {
 
 const modal = document.querySelector('#modal');
 const openModal = document.querySelector('.add-button');
-const closeModal = document.querySelector('.close-button')
+const closeModal = document.querySelector('.close-button');
+const submitButton = document.querySelector('button[type="submit"]');
 
 openModal.addEventListener('click', () => {
     modal.showModal();
@@ -27,3 +28,23 @@ closeModal.addEventListener('click', () => {
     modal.close();
 })
 
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    let form = document.querySelector("form");
+
+    if (form.checkValidity()) {
+        form.reportValidity();
+
+        let title = document.querySelector('#title').value;
+        let author = document.querySelector('#author').value;
+        let pages = document.querySelector('#pages').value;
+        let isRead = document.querySelector('#read').checked;
+
+        let book = new Book(title, author, pages, isRead);
+        addBookToLibrary(book);
+        form.reset();
+        modal.close();
+    } else {
+        form.reportValidity();
+    }
+})
